035dbddca1e015335025ab2d165acf3940d9534a
tomatoraub <tomatoraub@tomatorau>
2023-11-22 00:43:26 +0900

    2023-11-22 00:43

---
+++ OOP/src/Java_Section04/AccessModifier.java
@@ -0,0 +1,51 @@
+/* MEMO
+ * 접근제한자
+ * private : 비공개(외부 접근을 막는 키워드)
+ * default : 같은 패키지 내 공개
+ * protected : 상속 범위 공개
+ * public : 공개
+ * 
+ * getter : 비공개 필드를 우회하여 가져옴
+ * setter : 비공개 필드를 우회하여 변경함
+ */
+
+/* 객체의 정보를 외부로부터 보호하지 않는 경우, 데이터가 함부로 변경될 수 있다.
+ * 이러한 문제를 예방하기 위해, 적절한 접근 제한자가 필요하다.
+ */
+
+package Java_Section04;
+public class AccessModifier {
+	public static void main(String[] args) {
+		
+		// 계좌 생성
+		Account myAccount = new Account(1000000);
+		System.out.println("해킹 전 "+ myAccount.toStr());
+		
+		// 계좌 해킹
+		Hacker.malcious(myAccount);
+		
+		// 계좌 해킹 후 결과 출력
+		System.out.println("해킹 후 "+ myAccount.toStr());
+	}
+	
+}
+
+class Account {
+	
+	int balance; // 디폴트 접근 제한자 -> 같은 공간 클래스에게 공개
+	
+	public Account(int balance) {
+		this.balance = balance;
+	}
+	
+	public String toStr() {
+		return "계좌 잔액 : " + this.balance;
+	}
+}
+
+class Hacker {
+	
+	public static void malcious(Account account) {
+		account.balance = 0;
+	}
+}
---
+++ OOP/src/Java_Section04/Private.java
@@ -0,0 +1,43 @@
+/*
+ * 외부로 부터 감추기 - private
+ */
+package Java_Section04;
+
+public class Private {
+	public static void main(String[] args) {
+		
+		// wallet 객체 생성
+		Wallet wallet = new Wallet(30000);
+		System.out.println(wallet.toString());
+		
+		// 도둑에게 지갑을 도난 당함
+		Thief.steal(wallet);
+		
+		// 결과 확인
+		System.out.println(wallet.toString());
+	}
+}
+
+class Wallet {
+	
+	// int money;
+	private int money; // 필드를 외부로 부터 감추기 위해 접근 제한자 private 사용
+	
+	public Wallet(int money) {
+		this.money = money;
+	}
+
+	@Override
+	public String toString() {
+		return "Wallet [money=" + money + "]";
+	}
+
+}
+
+class Thief {
+	
+	public static void steal(Wallet wallet) {
+		// wallet 클래스의 money 필드는 private라 접근 불가
+		// wallet.money = 0; 
+	}
+}
